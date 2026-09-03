504c621cde85257802c6c7cc2d71cb8a5c157378
chirpyli <s_lisheng@163.com>
2026-09-03 19:53:02 +0800

    rm extension pgstattuple

--- contrib/pgstattuple/pgstattuple--1.0--1.1.sql
+++
@@ -1,5 +0,0 @@
-/* contrib/pgstattuple/pgstattuple--1.0--1.1.sql */
-
--- complain if script is sourced in psql, rather than via ALTER EXTENSION
-\echo Use "ALTER EXTENSION pgstattuple UPDATE TO '1.1'" to load this file. \quit
-
